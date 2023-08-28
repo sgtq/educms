923ed33d1dda932dd64717570b8c40b08e2582b2
Gaston <gaston.quittner@gmail.com>
2023-08-28 19:26:01 -0400

    Init commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@/': path.resolve(__dirname, '/src/'),
+        },
+    },
+    server: {
+        hmr: {
+            host: '127.0.0.1',
+        },
+    }
+});
